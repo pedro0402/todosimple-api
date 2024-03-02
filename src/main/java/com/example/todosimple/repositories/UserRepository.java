aefb425dcf099b32d46ff514779ccacd49f486b4
pedro0402 <pedro693.ph@gmail.com>
2024-03-02 12:12:08 -0300

    Create User repository

---
+++ src/main/java/com/example/todosimple/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.todosimple.repositories;
+
+import com.example.todosimple.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
